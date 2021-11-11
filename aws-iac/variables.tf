bf5be3aed5f742184202a0b021acfe89a6c5e4db
kountey-wipro <94118821+kountey-wipro@users.noreply.github.com>
2021-11-11 19:24:28 +0530

    Add files via upload

---
+++ aws-iac/variables.tf
@@ -0,0 +1,10 @@
+
+variable "dome9_access_id" {
+  type        = string
+  default  = "37efab65-f2e1-4357-a5bd-c0ba1415d177"
+}
+
+variable "dome9_secret_key" {
+   type        = string
+  default  = "9sr8f51inx195m3t4dyppzqu"
+}
